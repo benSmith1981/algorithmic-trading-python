c3789176f4b6e495dc60791e87edbf4d06e53c4c
Ben Smith <benjaminsmith1981@googlemail.com>
2022-10-01 20:57:14 +0000

    Added api call to apple stock price with correct secret toke

--- trade.py
+++ trade.py
@@ -14,8 +14,10 @@ import requests
 from secrets import IEX_CLOUD_API_TOKEN
 stocks = pd.read_csv('sp_500_stocks.csv')
 
-symbol = 'APPL'
-#api_url = f'https://sandbox.iexapis.com/stable/stock/{symbol}/quote?token={IEX_CLOUD_API_TOKEN}'
-api_url = f'https://algorithmpython.iex.cloud/v1/data/CORE/HISTORICAL_PRICES?last=1&token={IEX_CLOUD_API_TOKEN}'
+symbol = 'AAPL'
+api_url = f'https://sandbox.iexapis.com/stable/stock/{symbol}/quote?token={IEX_CLOUD_API_TOKEN}'
+#api_url = f'https://algorithmpython.iex.cloud/v1/data/CORE/HISTORICAL_PRICES?last=1&token={IEX_CLOUD_API_TOKEN}'
+#api_url = f'https://algorithmpython.iex.cloud/v1/data/CORE/{symbol}/quote?token={IEX_CLOUD_API_TOKEN}'
+#api_url = 'https://sandbox.iexapis.com/stable/stock/AAPL/quote?token=Tpk_059b97af715d417d9f49f50b51b1c448'
 data = requests.get(api_url).json()
 print(data)
